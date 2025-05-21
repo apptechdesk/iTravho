9c3be5724cb53e85f509c70cea9d6f910f6244ca
Sivamani-18 <sivasubramaniyam09@gmail.com>
2025-05-21 13:48:32 +0000

    Synced exported static site from TravhoBooking main: Wed May 21 13:48:32 UTC 2025

--- site/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  basePath:
-    process.env.NODE_ENV === 'production' ? process.env.NEXT_APP_BASENAME : '',
-  trailingSlash: true,
-};
-
-export default nextConfig;
